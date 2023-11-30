8812424fd408b515d0f3ac045c680196a606a098
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-11-30 23:10:45 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,8 +40,8 @@ export default function Home() {
         />
       </div>
       <div className="flex flex-row py-6">
-        <Link href={"/Dashboard"}> Dashboard</Link>
-        <Link href={"/setting"}> Setting</Link>
+        <Link href={"/Dashboard"} className="py-5"> Dashboard</Link>
+        <Link href={"/setting"} className="py-5"> Setting</Link>
       </div>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a

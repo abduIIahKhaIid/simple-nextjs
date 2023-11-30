e136678af57c11301cf42526295761aaffd99fd4
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-11-30 23:07:31 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -37,6 +38,10 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+      <div className="flex flex-row py-6">
+        <Link href={"/Dashboard"}> Dashboard</Link>
+        <Link href={"/setting"}> Setting</Link>
       </div>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
